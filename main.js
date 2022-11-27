45cae1e84434fa316dc4dde56d8f639479d8dd81
rajwardhanspimple <85007388+rajwardhanspimple@users.noreply.github.com>
2022-11-27 19:24:17 +0530

    Create main.js

---
+++ main.js
@@ -0,0 +1,9 @@
+// Connect to the socket server
+var socket = io();
+
+// Get the message form
+var messageForm = document.getElementById('message-form');
+
+// Get the message input field
+var messageInput = document.getElementById('message-input');
+
